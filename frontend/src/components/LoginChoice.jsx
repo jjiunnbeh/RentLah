ecaa4a55e17c7f86d5565046b1da6ce41910bf73
jjiunnbeh <jjiunnbeh@gmail.com>
2024-03-11 22:45:10 +0800

    Add LoginChoice and RegisterChoice

--- frontend/src/components/LoginChoice.jsx
+++ frontend/src/components/LoginChoice.jsx
@@ -1,5 +1,5 @@
 import "../styles/Choice.css";
-
+import { Link } from 'react-router-dom';
 
 
 function LoginChoice()
--- frontend/src/components/RegisterChoice.jsx
+++ frontend/src/components/RegisterChoice.jsx
@@ -1,5 +1,5 @@
 import "../styles/Choice.css";
-
+import { Link } from 'react-router-dom';
 
 
 function RegisterChoice()
@@ -9,8 +9,8 @@ function RegisterChoice()
     
     return (
         <div className="d-grid gap-2">
-  <button onClick={()=>{console.log("Login as Customer")}}className="btn btn-primary" type="button">Register as Customer</button>
-  <button onClick={()=>{console.log(" as Agent")}}className="btn btn-primary" type="button">Register as Agent</button>
+  <button onClick={()=>{console.log("Reg as Customer")}}className="btn btn-primary" type="button">Register as Customer</button>
+  <button onClick={()=>{console.log("Reg as Agent")}}className="btn btn-primary" type="button">Register as Agent</button>
   <a onClick={()=>{console.log("LoginChoice")}} href="/">Have account? Login Here</a>
         </div>
     )
