f4d9e199382f7a3b2f97b602ddaa551990fdf8b9
Phan Nguyen <94454067+phanng0605@users.noreply.github.com>
2024-03-15 17:29:39 +0800

    Update login.js

--- backend/function/login.js
+++ backend/function/login.js
@@ -120,7 +120,7 @@ async function connectToDatabase() { //preferably mongoDB
    * Connection URI. Replace placeholders with your actual credentials.
    * Refer to https://docs.mongodb.com/ecosystem/drivers/node/ for details.
    */
-  const uri = "mongodb+srv://yamemtid69:SC2006PROJECT@sc2006project.1tavfjr.mongodb.net/?retryWrites=true&w=majority&appName=SC2006Project";
+  const uri = "hehe";
   const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
   try {
