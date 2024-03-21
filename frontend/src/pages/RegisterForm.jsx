f896dda2bcc9c7c664d631c8ca69f5ebcdfea77c
SakaJoe <SAKA0001@e.ntu.edu.sg>
2024-03-21 18:59:00 +0800

    Create Registration page for agent and customer

--- frontend/src/pages/RegisterForm.jsx
+++ frontend/src/pages/RegisterForm.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import "../styles/LoginForm.css";
+import "../styles/RegisterForm.css";
 import Axios from "axios";
 import {loginService} from "../service/LoginService";
 import useSignIn from 'react-auth-kit/hooks/useSignIn';
@@ -20,7 +20,7 @@ function RegisterForm({ user })
         <>
             <div className="formcontainer">
                 <form name={user} onSubmit={handleSubmit}>
-                    <h1 className="text-center">Agent Registration</h1>
+                    <h1 className="text-center font-weight-bold" style={{color:"white"}}>{user} Registration</h1>
                     <div className="row justify-content-center">
                         <label htmlFor="inputUserName3" className="col-sm-8 col-form-label ">
                             Username
@@ -38,6 +38,130 @@ function RegisterForm({ user })
                             />
                         </div>
                     </div>
+                    <br></br>
+                    <div className="row justify-content-center">
+                        <label htmlFor="inputEmail" className="col-sm-8 col-form-label ">
+                            Email Address
+                        </label>
+                        <div className="col-sm-8">
+                            <input
+                            type="email"
+                            className="form-control"
+                            id="inputEmail"
+                            placeholder="Email Address"
+                            onChange={handleChange}
+                            name="email"
+                            onKeyDown= {(event)=> (event.key === "Enter" || event.key ===" ") && event.preventDefault()}
+                            required
+                            />
+                        </div>
+                    </div>
+                    {user=="Agent" &&
+                    <>
+                        <br></br>
+                        <div className="row justify-content-center">
+                            <label htmlFor="inputAgentName" className="col-sm-8 col-form-label ">
+                            Agent Name
+                            </label>
+                                <div className="col-sm-8">
+                                <input
+                                type="text"
+                                className="form-control"
+                                id="inputAgentName"
+                                placeholder="Agent Name"
+                                onChange={handleChange}
+                                name="agentname"
+                                onKeyDown= {(event)=> (event.key === "Enter" || event.key ===" ") && event.preventDefault()}
+                                required
+                                />
+                            </div>
+                    </div>
+                        <br></br>
+                        <div className="row justify-content-center">
+                            <label htmlFor="inputAgentRegNum" className="col-sm-8 col-form-label ">
+                            Agent Registration Number
+                            </label>
+                                <div className="col-sm-8">
+                                <input
+                                type="text"
+                                className="form-control"
+                                id="inputAgentRegNum"
+                                placeholder="Agent Registration Number"
+                                onChange={handleChange}
+                                name="agentregnum"
+                                onKeyDown= {(event)=> (event.key === "Enter" || event.key ===" ") && event.preventDefault()}
+                                required
+                                />
+                            </div>
+                    </div>
+
+                    </>
+                    
+                        
+                        }
+                    <br></br>
+                    <div className="row justify-content-center">
+                        <label htmlFor="inputPhoneNumber" className="col-sm-8 col-form-label ">
+                            Phone Number
+                        </label>
+                        <div className="col-sm-8">
+                                <input
+                                type="tel"
+                                className="form-control"
+                                id="inputPhoneNumber"
+                                placeholder="Phone Number"
+                                onChange={handleChange}
+                                name="phone number"
+                                onKeyDown= {(event)=> (event.key === "Enter" || event.key ===" ") && event.preventDefault()}
+                                required
+                                />
+                        </div>
+                    </div>
+                    <br></br>
+                    <div className="row justify-content-center">
+                        <label htmlFor="inputPassword" className="col-sm-8 col-form-label ">
+                            Password
+                        </label>
+                        <div className="col-sm-8">
+                            <input
+                            type="password"
+                            className="form-control"
+                            id="inputPassword"
+                            placeholder="Password"
+                            onChange={handleChange}
+                            name="password"
+                            onKeyDown= {(event)=> (event.key === "Enter" || event.key ===" ") && event.preventDefault()}
+                            required
+                            />
+                        </div>
+                    </div>
+                    <br></br>
+                    <div className="row justify-content-center">
+                        <label htmlFor="inputPasswordConfirmation" className="col-sm-8 col-form-label ">
+                            Password Confirmation
+                        </label>
+                        <div className="col-sm-8">
+                            <input
+                            type="password"
+                            className="form-control"
+                            id="inputPasswordConfirmation"
+                            placeholder="Password Confirmation"
+                            onChange={handleChange}
+                            name="password confirmation"
+                            onKeyDown= {(event)=> (event.key === "Enter" || event.key ===" ") && event.preventDefault()}
+                            required
+                            />
+                        </div>
+                    </div>
+                    <br></br>
+                    <br></br>
+                    <div className="row justify-content-center">
+                        <button type="submit" className="btn btn-primary loginSubmit" >
+                            Register
+                        </button>
+                    </div>
+
+
                 </form>
             </div>
             <div className="imagecontainer">
