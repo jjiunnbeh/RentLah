49a67274bdc53bffbdaeb496f70396596a785b71
SakaJoe <SAKA0001@e.ntu.edu.sg>
2024-03-28 18:50:10 +0800

    implemented homepage

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,10 +1,65 @@
 import NavBar from "../components/NavBar";
 import {useSelector} from "react-redux";
+import Triangles from "../components/Triangles";
+import "../styles/Home.css"
 
 function Home()
-{
+{   
+    const userType = useSelector((state) => state.user.currentUser.rest.userType);
+    console.log(userType);
+    const currentUser = useSelector((state) => state.user.currentUser.rest);
+
+
+
+
+
+
  return (
+
+    <>
     <header><NavBar/></header>
+
+    <Triangles />
+
+    <div className="row text-center"style={{marginTop: "3%"}} >
+        <h1> Hello {currentUser.username} </h1>
+    </div>
+
+    <div className="col overflow-auto" id="Listingsbar" style={{marginTop: "7%", width:"100%"}} >
+        <div className="row" style={{marginLeft: "6%"}} >
+            <h2> Listings</h2>
+        </div>
+
+        <div className="row d-flex gap-4 flex-nowrap" style={{marginLeft: "6%"}}>
+           {
+            [...Array(10)].map((e, i) => <div className="Listingcontainer" key={i}>
+                                            <div className="col-mx-auto d-grid gap-4" style={{width: "525px"}}>
+                                                <div className="row justify-content-center">
+                                                    <div className="Listingimg">
+                                                        
+                                                    </div>
+                                                </div>
+
+                                                <div className="row" style={{marginTop: "20px", width:"525px"}}>
+                                                    <h2 className="text-truncate"> Address: dskfjhsdjkfhajdshfkljsdhfjhsdafkjsadfkjdsakfhsdkjfhasdkjfsjkadhfkjsdfjksdfhkdlsajkjkadsfkhdslk</h2>
+                                                </div>
+                                                <div className="row">
+                                                    <h2> Pricing: </h2>
+                                                </div>
+                                                <div className="row">
+                                                    <a className="Listing" href=""> Learn more... </a>
+                                                </div>
+                                                    
+                                                <div className="row">
+                                                <a className="Listing" href=""> Add to watchlist... </a>
+                                                </div>
+                                            </div>
+                                        </div>)
+           }
+           <div className="col" style={{minWidth:"5%"}}/>
+        </div>
+    </div>
+    </>
  );
     
 
