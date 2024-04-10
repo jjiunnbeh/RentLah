572010dc0936b11cd2247a1a329b2fa6cbd25f58
jjiunnbeh <jjiunnbeh@gmail.com>
2024-04-10 15:59:48 +0800

    add to watchlist

--- frontend/src/components/Watchlist.jsx
+++ frontend/src/components/Watchlist.jsx
@@ -3,58 +3,62 @@ import Triangles from "../components/Triangles";
 import NavBar from "../components/NavBar";
 import "../styles/SearchResults.css";
 import PaginationComponent from "../components/PageNavigator";
+import { useSelector, useDispatch } from "react-redux";
+import axios from "axios"
 //import { fetchPropertyListings } from './propertyListings'; // Import the fetchPropertyListings function from your backend API file
 
-const PropertyListings = ({ listings }) => {
-    return (
-        <div>
-            <h2>Available Property Listings</h2>
-            <ul>
-                {listings.map((listing, index) => (
-                    <li key={index}>
-                        <p>{listing.address}</p>
-                        <p>{listing.price}</p>
-                        {/* all property details go here*/}
-                    </li>
-                ))}
-            </ul>
-        </div>
-    );
-};
+// const PropertyListings = ({ listings }) => {
+//     return (
+//         <div>
+//             <h2>Available Property Listings</h2>
+//             <ul>
+//                 {listings.map((listing, index) => (
+//                     <li key={index}>
+//                         <p>{listing.address}</p>
+//                         <p>{listing.price}</p>
+//                         {/* all property details go here*/}
+//                     </li>
+//                 ))}
+//             </ul>
+//         </div>
+//     );
+// };
 
-const SearchBar = () => {
-    const [searchQuery, setSearchQuery] = useState('');
+// const SearchBar = () => {
+//     const [searchQuery, setSearchQuery] = useState('');
 
-    const handleSearch = () => {
-        // Implement search functionality here
-    };
+//     const handleSearch = () => {
+//         // Implement search functionality here
+//     };
 
-    return (
-        <div className='input-group'>
-            <input className="form-control" type="text" value={searchQuery} onChange={(e) => setSearchQuery(e.target.value)} />
-            <button className="btn btn-dark" style={{fontSize:"25px"}} onClick={handleSearch}>🔍</button>
-        </div>
-    );
-};
+//     return (
+//         <div className='input-group'>
+//             <input className="form-control" type="text" value={searchQuery} onChange={(e) => setSearchQuery(e.target.value)} />
+//             <button className="btn btn-dark" style={{fontSize:"25px"}} onClick={handleSearch}>🔍</button>
+//         </div>
+//     );
+// };
 
 const Watchlist = () => {
     const [propertyListings, setPropertyListings] = useState([]);
     const [currentPage, setCurrentPage] = useState(1);
+    const currentUser = useSelector((state) => state.user.currentUser);
 
 
-    const listing1 = {
-        name:"PDR The Gardens at Your Mom's House",
-        postalCode:649823,
-        price:69.69,
-        description:"ARC",
-        address:'This is\nsupposed to be\nannnnnnnnnnnnnnnnnnnnnnnnnnnnnnnn ',
-        bedroom:3,
-        bathroom:2,
-        images:["https://firebasestorage.googleapis.com/v0/b/rentlah-667e3.appspot.com/o/1711687189301download%20(1).jpeg?alt=media&token=359100cb-2c18-4666-8ba8-ccc80c88e025","https://firebasestorage.googleapis.com/v0/b/rentlah-667e3.appspot.com/o/1711687215981download.jpeg?alt=media&token=d05befb6-d255-4bc9-b217-fe8e05ce5a45"],
-        agentRef:"agent1",
-        latitude: 1.4332513,
-        longitude: 103.7874458
-      };
+
+    // const listing1 = {
+    //     name:"PDR The Gardens at Your Mom's House",
+    //     postalCode:649823,
+    //     price:69.69,
+    //     description:"ARC",
+    //     address:'This is\nsupposed to be\nannnnnnnnnnnnnnnnnnnnnnnnnnnnnnnn ',
+    //     bedroom:3,
+    //     bathroom:2,
+    //     images:["https://firebasestorage.googleapis.com/v0/b/rentlah-667e3.appspot.com/o/1711687189301download%20(1).jpeg?alt=media&token=359100cb-2c18-4666-8ba8-ccc80c88e025","https://firebasestorage.googleapis.com/v0/b/rentlah-667e3.appspot.com/o/1711687215981download.jpeg?alt=media&token=d05befb6-d255-4bc9-b217-fe8e05ce5a45"],
+    //     agentRef:"agent1",
+    //     latitude: 1.4332513,
+    //     longitude: 103.7874458
+    //   };
 
     // useEffect(() => {
     //     // Fetch property listings when the component mounts
@@ -62,6 +66,24 @@ const Watchlist = () => {
     //         .then((listings) => setPropertyListings(listings))
     //         .catch((error) => console.error('Error fetching property listings:', error.message));
     // }, []);
+    useEffect(() => {
+        const BASE_URL = "http://localhost:3000";  
+        const fetchData = async () => {
+            try {
+                const response = await axios.get(`${BASE_URL}/api/listing/get-watchlist/${currentUser._id}`);
+                if (response.status == 200)
+                {
+                    setPropertyListings(response.data);
+
+                }
+            } catch (error) {
+                console.error(error);
+            }
+        };
+    
+        fetchData();
+    }, []);
+
 
     return (
         <>
@@ -82,23 +104,23 @@ const Watchlist = () => {
         </div>
 
         <div className="d-grid gap-3" style={{marginTop:"3%", marginLeft:"17%",marginRight:"17%"}}>
-            {
-                [...Array(10)].map((e,i) => <div className="row" key={i}>
+            
+        {propertyListings.slice(0, 10).map((listing) =>( <div className="row" key={listing._id}>
                                                     <div className='col-sm-auto'>
                                                         <div className="img-div">
-                                                            <img  src={listing1.images[0]}></img>
+                                                            <img  src={listing.images[0]}></img>
                                                         </div>
                                                     </div>
 
                                                     <div className="col d-grid mt-2 gap-2">
                                                         <div className="row" style={{width:"545px"}}>
-                                                            <h2 className="text-truncate"> {listing1.name} </h2>
+                                                            <h2 className="text-truncate"> {listing.name} </h2>
                                                         </div>
                                                         <div className="row" style={{width:"545px"}}>
-                                                            <h2 className="text-truncate"> Address: {listing1.address} </h2>
+                                                            <h2 className="text-truncate"> Address: {listing.address} </h2>
                                                         </div>
                                                         <div className="row" style={{width:"545px"}}>
-                                                        <h2> ${listing1.price} </h2>
+                                                        <h2> ${listing.price} </h2>
                                                         </div>
                                                     </div>
 
@@ -118,8 +140,8 @@ const Watchlist = () => {
                                                         </div>
 
                                                     </div>
-                                                </div>)
-            }
+                                                </div>))}
+            
         </div>
 
         <div className="row justify-content-center" style={{marginBottom:"3%", marginTop:"3%"}}>
